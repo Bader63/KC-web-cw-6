ca1574ef5df346c468f42497f571c50a2452659f
Bader63 <c.baderamg@icloud.com>
2022-10-28 13:25:03 +0300

    done

---
+++ sccript.js
@@ -0,0 +1,20 @@
+alert("مرحبا بك في موقعي الذي سيحسب دراجاتك");
+//to do grade
+prompt("83");
+console.log("bader");
+//todo if statment
+let mark = prompt("دراجات");
+
+if ("90-100") {
+  console.log("حصلت علي امتياز");
+} else if ("80 - 89") {
+  console.log("لقد حصلت على جيدجدا");
+} else if ("70-70") {
+  console.log("لقد حصلت على جيد");
+} else if ("60-69") {
+  console.log("لقد حصلت على مقبول");
+} else if ("50-50") {
+  console.log("لقد حصلت على ضغيف");
+} else if (">50") {
+  console.log("راسب");
+}
